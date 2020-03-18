ec5af8dbdd2a2fb277f43f2635bc86df2968438d
Aleksandr <alx8437@mail.ru>
2020-03-18 18:00:39 +0300

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://i.pinimg.com/originals/72/52/3d/72523df85c330062933916e603680cde.png"></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://openimagedenoise.github.io/images/moana_16spp_oidn.jpg"></img>
+            </div>
+            <div>
+                <img scr="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg"></img>
+            </div>
+
+        </div>
+    )
+};
+
+export default Profile;
