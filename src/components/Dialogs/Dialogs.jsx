aa721b8726b3930ce4d3748bbdbd10bd411d5954
Aleksandr <alx8437@mail.ru>
2020-03-24 18:09:12 +0300

    add section messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,33 @@ import classes from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItem}>
+                <div className={classes.dialog}>
+                    Juli
+                </div>
+                <div className={classes.dialog}>
+                    Nataly
+                </div>
+                <div className={classes.dialog}>
+                    Robert
+                </div>
+                <div className={classes.dialog}>
+                    John
+                </div>
+                <div className={classes.dialog}>
+                    Thomas
+                </div>
+                <div className={classes.dialog}>
+                    Nadin
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hello!</div>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you?</div>
+            </div>
+
         </div>
     )
 };
