b958b3505f2d860fe92a1c2206435955d1811bea
Alx8437 <alx8437@mail.ru>
2020-06-04 18:22:08 +0300

    end added User component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,19 +2,67 @@ import React from "react";
 import styles from "./Users.module.css"
 
 const Users = (props) => {
+
+    if (props.users.length === 0) {
+        props.setUsers(
+            [
+                {
+                    id: 1,
+                    fotoUrl: 'https://mult.tv/images/super-krylya.webp',
+                    fullName: 'Alex',
+                    followed: true,
+                    status: 'student',
+                    location: {city: "Krasnodar", country: "Russian"}
+                },
+                {
+                    id: 2,
+                    fotoUrl: 'https://mult.tv/images/super-krylya.webp',
+                    fullName: 'John',
+                    followed: false,
+                    status: 'student',
+                    location: {city: "New-York", country: "USA"}
+                },
+                {
+                    id: 3,
+                    fotoUrl: 'https://mult.tv/images/super-krylya.webp',
+                    fullName: 'Mia',
+                    followed: true,
+                    status: 'student',
+                    location: {city: "Berlin", country: "Germany"}
+                },
+                {
+                    id: 4,
+                    fotoUrl: 'https://mult.tv/images/super-krylya.webp',
+                    fullName: 'Zofia',
+                    followed: false,
+                    status: 'student',
+                    location: {city: "Warsaw", country: "Poland"}
+                }
+            ]
+        );
+    }
+
+
     return (
         <div>
             {
-                props.users.map(u =>  <div key={u.id}>
+                props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
                             <img className={styles.userPhoto} src={u.fotoUrl}/>
                         </div>
                         <div>
-                            {u.followed ? <button>Follow</button> : <button>Unfollow</button>}
+                            {u.followed
+                                ? <button onClick={() => {
+                                    props.followUser(u.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.unFollowUser(u.id)
+                                }}>Follow</button>
+                            }
                         </div>
                     </span>
-                    <span>
+                        <span>
                         <span>
                             <div>{u.fullName}</div>
                             <div>{u.status}</div>
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -4,12 +4,7 @@ const SET_USERS = "SET_USERS";
 
 
 let initialState = {
-    users: [
-        {id: 1, fotoUrl: 'https://mult.tv/images/super-krylya.webp', fullName: 'Alex', followed: true, status: 'student', location: {city: "Krasnodar", country: "Russian"}},
-        {id: 2, fotoUrl: 'https://mult.tv/images/super-krylya.webp', fullName: 'John', followed: false, status: 'student', location: {city: "New-York", country: "USA"}},
-        {id: 3, fotoUrl: 'https://mult.tv/images/super-krylya.webp', fullName: 'Mia', followed: true, status: 'student', location: {city: "Berlin", country: "Germany"}},
-        {id: 4, fotoUrl: 'https://mult.tv/images/super-krylya.webp', fullName: 'Zofia', followed: false, status: 'student', location: {city: "Warsaw", country: "Poland"}}
-    ]
+    users: []
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -19,7 +14,7 @@ const usersReducer = (state = initialState, action) => {
               ...state,
               users: state.users.map(user => {
                   if (user.id === action.userId) {
-                      return {...user, followed: true}
+                      return {...user, followed: false}
                   }
                   return user
               })
@@ -29,7 +24,7 @@ const usersReducer = (state = initialState, action) => {
                 ...state,
                 users: state.users.map(user => {
                     if (user.id === action.userId) {
-                        return {...user, followed: false}
+                        return {...user, followed: true}
                     }
                     return user
                 })
